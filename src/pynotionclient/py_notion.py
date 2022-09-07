97979bde1cdebbeb42e21648c2b5a60c5d83d48f
Vetrichelvan <pythonhub.py@gmail.com>
2022-09-08 00:00:06 +0530

    More pylint rules along with mypy package
    Added more custom pylint rules to maintain code standards, also added mypy package for static type checking to maintain type hinting all over the project

--- src/pynotionclient/py_notion.py
+++ src/pynotionclient/py_notion.py
@@ -6,9 +6,13 @@ class PyNotion:
     def __init__(self, token: str):
         function_name = "Intialize PyNotion Client"
         # Initialize PyNotion client with SECRET_TOKEN or API_TOKEN from Notion
-        self.token = token
+        self.token: str = 1
         if token is None or len(token) == 0:
             # Raise error if token is not provided
             logger.error(message="Invalid API token provided", function_name=function_name, file_name="py_notion.py")
             raise InvalidTokenException
         logger.info(message="Successfully intialized PyNotion Client", function_name=function_name, file_name="py_notion.py")
+
+    def sample(self) -> int:
+        print(self.token)
+        return "a"
