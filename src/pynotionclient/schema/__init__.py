340b846a6cfcb736d9bffc897af4ad1a2819316e
Vetrichelvan <pythonhub.py@gmail.com>
2022-10-02 09:39:02 +0530

    Modified imports

--- src/pynotionclient/schema/__init__.py
+++ src/pynotionclient/schema/__init__.py
@@ -1,5 +1,5 @@
 from src.pynotionclient.schema.common.header_schema import HeaderSchema
-from src.pynotionclient.schema.common.header_schema import HeaderSchema, default_header_schema
+from src.pynotionclient.schema.common.header_schema import default_header_schema
 from src.pynotionclient.schema.database.check_box_schema import CheckboxSchema
 from src.pynotionclient.schema.database.common_filter_schema import EqualsFilter, EmptyFilter, ContainsFilter
 from src.pynotionclient.schema.database.common_info_schema import IdTypeSchema, IdTypeNameSchema, IdNameSchema, IDSchema
@@ -25,7 +25,6 @@ from src.pynotionclient.schema.database.other_filters_schema import (
     RelationFilter,
 )
 from src.pynotionclient.schema.database.person_schema import PersonEmailSchema, PersonSchema
-from src.pynotionclient.schema.database.person_schema import PersonSchema
 from src.pynotionclient.schema.database.result_schema import ResultSchema
 from src.pynotionclient.schema.database.rich_text_filter_schema import RichTextFilter
 from src.pynotionclient.schema.database.rich_text_schema import RichTextSchema
@@ -33,3 +32,40 @@ from src.pynotionclient.schema.database.select_schema import SelectSchema
 from src.pynotionclient.schema.database.status_schema import StatusSchema
 from src.pynotionclient.schema.database.time_stamp_filter_schema import TimeStampFilter
 from src.pynotionclient.schema.database.title_schema import TitleSchema
+
+__all__ = [
+    "NotionDatabaseResponseSchema",
+    "IdTypeSchema",
+    "IdTypeNameSchema",
+    "TitleSchema",
+    "PersonSchema",
+    "PersonEmailSchema",
+    "CheckboxSchema",
+    "IdNameSchema",
+    "IDSchema",
+    "ResultSchema",
+    "StatusSchema",
+    "ContentSchema",
+    "SelectSchema",
+    "MultiSelectSchema",
+    "NumberSchema",
+    "RichTextSchema",
+    "Filter",
+    "EmptyFilter",
+    "RichTextFilter",
+    "ContainsFilter",
+    "EqualsFilter",
+    "PropertyFilter",
+    "CompoundFilterOR",
+    "CompoundFilterAND",
+    "CheckboxFilter",
+    "NumberFilter",
+    "DateFilter",
+    "PeopleFilter",
+    "StatusFilter",
+    "FileFilter",
+    "SelectFilter",
+    "RelationFilter",
+    "MultiSelectFilter",
+    "TimeStampFilter",
+]
